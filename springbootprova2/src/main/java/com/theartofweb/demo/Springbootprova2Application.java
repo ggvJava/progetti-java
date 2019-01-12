35a3cbf25fee946a22b81d9473fdd5a634b059ad
Gian <giangaetano@giangaetano-HP-Pavilion-dv6-Notebook-PC>
2019-01-12 20:49:46 +0100

    init commit

---
+++ springbootprova2/src/main/java/com/theartofweb/demo/Springbootprova2Application.java
@@ -0,0 +1,14 @@
+package com.theartofweb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootprova2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootprova2Application.class, args);
+	}
+
+}
+
